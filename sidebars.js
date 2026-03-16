18ab37f8dcdac368af1cfd80fe2f7fe71731c403
Joe <business.tytuslemon@gmail.com>
2026-03-16 14:23:33 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,14 @@
-module.exports = {
-  docs: [
-    'penalties',
-    'traffic-laws',
-    'safe-driving',
-    'faq'
-  ]
-};
+{
+  "name": "drivesafe",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "build": "docusaurus build",
+    "start": "serve -s build -l $PORT"
+  },
+  "dependencies": {
+    "@docusaurus/core": "^3.0.0",
+    "@docusaurus/preset-classic": "^3.0.0",
+    "serve": "^14.2.0"
+  }
+}
